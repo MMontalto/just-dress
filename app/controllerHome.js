722a04fc5671169ecdd28ded4b266f0fbadd41fe
Luca L P. Provini <l.provini@MEDIAENG.local>
2016-11-17 11:24:48 +0100

    aggiunta angular

---
+++ app/controllerHome.js
@@ -0,0 +1,12 @@
+angular.module('AppControllers',[])
+.controller('controllerHome', [ '$scope', '$http', '$window',
+  function ($scope,$http,$window ) {
+
+  init();
+
+  function init(){
+    console.log('ci sono!');
+  }
+
+  }]
+);
